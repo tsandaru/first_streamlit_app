b704820940bee81f522820d96b6abcba1064e2e9
tsandaru <thisalst@gmail.com>
2023-02-22 23:45:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 
-streamlit.title('I Love my daughter')
-streamlit.header('Shez name is Camellia')
-
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
